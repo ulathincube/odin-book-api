8bf84537f81a9ea88ba383051b7735dd10c7b124
Ulathi Ncube <lathincube@gmail.com>
2026-09-18 17:10:14 +0200

    Feat: add routing to app

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { ORIGIN } from "./utils/constants.js"
 import notFound from "./errors/notFound.js"
 import errorHandler from "./errors/errorHandler.js"
 import CustomError from "./errors/customError.js"
+import messageRouter from "./routes/message.js"
+import postRouter from "./routes/post.js"
+import profileRouter from "./routes/profile.js"
+import userRouter from "./routes/user.js"
 
 if (!ORIGIN)
   throw new CustomError(500, "--Origin URL not provided: Server exiting--")
@@ -16,12 +20,17 @@ app.use(
     origin: ORIGIN,
     credentials: true,
     methods: ["GET", "POST"],
-  }),
+  })
 )
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("tiny"))
+
+app.use("/api/messages", messageRouter)
+app.use("/api/posts", postRouter)
+app.use("/api/users", userRouter)
+app.use("/api/profiles", profileRouter)
 
 app.use("/{*splat}", notFound)
 app.use(errorHandler)
